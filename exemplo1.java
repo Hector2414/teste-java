ea20f032098152ff0497497acae0b8680fad41f5
Hector2414 <123740452+Hector2414@users.noreply.github.com>
2024-05-27 11:59:44 -0300

    Add files via upload

---
+++ exemplo1.java
@@ -0,0 +1,20 @@
+package aula1;
+
+public class exemplo1 {
+
+	public static void main(String[] args) {
+
+
+		String primeiroNome = "Maria";
+		String sobrenome = "Silva";
+		int idade = 40;
+		char sexo = 'F';
+		float salario = 10245.56f;
+		
+		System.out.println(primeiroNome +' '+ sobrenome);
+		System.out.println(primeiroNome +' '+ idade);
+		System.out.println(primeiroNome +' '+ idade +' '+ salario);
+
+	}
+
+}
